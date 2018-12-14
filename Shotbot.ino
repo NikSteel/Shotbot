fc3d5c58a8564c9a8420fe763f88d56677a7873f
Nik Steel <nikolassteel@gmail.com>
2018-12-13 21:36:03 -0500

    update pin/chip assignments

--- Shotbot.ino
+++ Shotbot.ino
@@ -14,8 +14,8 @@ const int maxPosition = 10;
 
 void setup() {
   //set the I2C address for MCP chip A and B
-  mcpA.begin(0);
-  mcpB.begin(1);
+  mcpA.begin(1);
+  mcpB.begin(0);
 
   //Set the button input pins
   Button_Setup();
@@ -52,16 +52,16 @@ void loop() {
 
 int Valve_GetPin(int position) {
   switch (position) {
-    case 1:  return 1;
-    case 2:  return 2;
-    case 3:  return 3;
-    case 4:  return 4;
-    case 5:  return 5;
-    case 6:  return 6;
-    case 7:  return 7;
-    case 8:  return 8;
-    case 9:  return 9;
-    case 10: return 10;
+    case 1:  return 9;
+    case 2:  return 10;
+    case 3:  return 11;
+    case 4:  return 12;
+    case 5:  return 13;
+    case 6:  return 14;
+    case 7:  return 15;
+    case 8:  return 16;
+    case 9:  return 7;
+    case 10: return 8;
   }
 }
 
@@ -120,20 +120,20 @@ int Light_GetPin(int position) {
     case 3:  return 13;
     case 4:  return 14;
     case 5:  return 15;
-    case 6:  return 11;
-    case 7:  return 12;
-    case 8:  return 13;
-    case 9:  return 14;
-    case 10: return 15;
+    case 6:  return 16;
+    case 7:  return 5;
+    case 8:  return 6;
+    case 9:  return 7;
+    case 10: return 8;
   }
 }
 
 Adafruit_MCP23017& Light_GetChip(int position) {
-  if (position <=5) {
-    return mcpA;
+  if (position <=6) {
+    return mcpB;
   }
   else {
-    return mcpB;
+    return mcpA;
   }
 }
 
@@ -173,14 +173,14 @@ void Button_Setup() {
 
 int Button_GetPin(int position) {
   switch (position) {
-    case 1:  return 1;
-    case 2:  return 2;
-    case 3:  return 3;
-    case 4:  return 4;
-    case 5:  return 5;
-    case 6:  return 6;
-    case 7:  return 7;
-    case 8:  return 8;
+    case 1:  return 5;
+    case 2:  return 6;
+    case 3:  return 7;
+    case 4:  return 8;
+    case 5:  return 1;
+    case 6:  return 2;
+    case 7:  return 3;
+    case 8:  return 4;
     case 9:  return 9;
     case 10: return 10;
   }
